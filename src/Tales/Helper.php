681b488b97bd55171c3b41afb0a4a11221ee1083
Xorock <aaatoja@o2.pl>
2016-07-18 16:28:43 +0200

    Fix whitespace

--- src/Tales/Helper.php
+++ src/Tales/Helper.php
@@ -9,16 +9,16 @@ class Helper implements PHPTAL_Tales
 {
     /**
      * Tal extension to allow helper invoke.
-	 *
-	 * Example use within template: <a tal:attributes="href helper:url('home')">Link</a>
+     *
+     * Example use within template: <a tal:attributes="href helper:url('home')">Link</a>
      * 
      * @param string $src     The original template string.
-	 * @param bool   $nothrow Whether to throw an exception on error.
+     * @param bool   $nothrow Whether to throw an exception on error.
      * @return mixed
      */
     public static function helper($src, $nothrow)
     {
-        $src = 'helper->'.trim($src);
+        $src = 'helper->' . trim($src);
         return PHPTAL_Php_Transformer::transform($src, '$ctx->');
     }
 }
